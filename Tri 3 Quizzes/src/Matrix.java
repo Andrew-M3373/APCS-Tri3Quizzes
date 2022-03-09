a79fb6bc375e3601fdde41fbaa35d1692ad9c02e
Andrew <Andrew@AComputer>
2022-03-08 19:54:54 -0700

    updated formatting

--- Tri 3 Quizzes/src/Matrix.java
+++ Tri 3 Quizzes/src/Matrix.java
@@ -4,31 +4,32 @@ public class Matrix {
 	public static void main(String[] args) {
 
 		int[][] matrix = new int[6][6];
-		for(int r = 0; r < matrix.length; r++) {
-			for(int c = 0; c < matrix[r].length; c++) {
-				matrix[r][c] = (int) (Math.random() * 10 +1);
+		for (int r = 0; r < matrix.length; r++) {
+			for (int c = 0; c < matrix[r].length; c++) {
+				matrix[r][c] = (int) (Math.random() * 10 + 1);
 			}
 		}
-		for(int r = 0; r < matrix.length; r++) {
-			for(int c = 0; c < matrix[r].length; c++) {
+		for (int r = 0; r < matrix.length; r++) {
+			for (int c = 0; c < matrix[r].length; c++) {
 				System.out.print(matrix[r][c] + " ");
 			}
 			System.out.println();
 		}
-		
+
 		System.out.println(findSmallest(matrix));
 	}
-	
+
 	public static int findSmallest(int[][] numbers) {
 		int smallest = numbers[0][0];
-		for(int r = 0; r < numbers.length; r++) {
-			for(int c = 0; c < numbers[r].length; c++) {
-				if (numbers[r][c] < smallest) smallest = numbers[r][c];
+		for (int r = 0; r < numbers.length; r++) {
+			for (int c = 0; c < numbers[r].length; c++) {
+				if (numbers[r][c] < smallest)
+					smallest = numbers[r][c];
 			}
 		}
-		
+
 		return smallest;
-		
+
 	}
 
 }
